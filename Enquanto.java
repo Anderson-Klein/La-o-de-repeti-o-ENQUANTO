5d2256b77c06a240e567f8e42c2fa5957a88a4e5
Anderson-Klein <160682630+Anderson-Klein@users.noreply.github.com>
2024-04-12 22:58:52 -0300

    Create Enquanto.java

---
+++ Enquanto.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        //Exemplo while (ENQUANTO)
+
+        int contador = 0;
+
+        //while (condição)
+        while (contador < 10) {
+            System.out.println(contador);
+            contador = contador + 1;
+        }
+
+        Scanner sc = new Scanner(System.in);
+        int opcao = 1;
+
+        /*whilw (true){
+
+            System.out.printf("Informe o valor");
+            opcao = sc.nextInt();
+
+            if (opcao == 0){
+                break;
+            }
+        }*/
+
+        while (opcao != 0) {
+            System.out.printf("Informe um valor");
+            opcao = sc.nextInt();
+        }
+    }
+}
