c2f5a307efcbe9317eaa8ea529353c21a0c101c0
Anderson-Klein <160682630+Anderson-Klein@users.noreply.github.com>
2024-04-13 00:08:23 -0300

    Create Vetor.java

---
+++ Vetor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        int[] vetor = new int[5];
+        Scanner sc = new Scanner(System.in);
+
+        for (int x = 0; i < 5; x++) {
+            vetor[x] = sc.nextInt();
+        }
+    }
+}
